256676cab7e478fbf10e817cc60ac383c32a986f
Nguyễn Phạm Đăng Quang <148599598+wandz2810@users.noreply.github.com>
2025-07-04 05:48:06 +0700

    ee

--- CathSpeak.Web/obj/Debug/net8.0/CathSpeak.Web.AssemblyInfo.cs
+++ CathSpeak.Web/obj/Debug/net8.0/CathSpeak.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CathSpeak.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0edcd2671197c64a98e5b6fca4dcb6b24a79ddd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1913401cb363f2493cb313d81eb550a35086c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("CathSpeak.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CathSpeak.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
